2f91077f48f8b37433f3c3bf5db2fe8dfec82d22
San <sganis@gmail.com>
2018-11-06 10:05:05 +0300

    Added key service

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,16 @@
 import json
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def root():
 	return (
 		"This app is working!<br/>"
 		"Get the servce: <a href='/getkey'>Get Key</a>"
 	)
 
-@app.route("/getkey")
+@application.route("/getkey")
 def getkey():
 	data = {
 		'user': 'some-user',
@@ -19,7 +19,7 @@ def getkey():
 	return json.dumps(data)
 
 if __name__ == "__main__":
-	app.run()
+	application.run()
 
 # openshift webhook
 # https://master.aramco-6a76.openshiftworkshop.com:443/apis/build.openshift.io/v1/namespaces/os-flask/buildconfigs/os-flask/webhooks/65ab01535f80037f/github
