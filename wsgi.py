e0555775db11eb719587917c8c887dc1ed372188
San <sganis@gmail.com>
2018-11-06 09:46:27 +0300

    Added json service

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,22 @@
+import json
 from flask import Flask
-application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+	return (
+		"This app is working!<br/>"
+		"Get the servce: <a href='/getkey'>Get Key</a>"
+	)
+
+@app.route("/getkey")
+def getkey():
+	data = {
+		'user': 'some-user',
+		'key' : 'Te98w@iim$#'
+	}
+	return json.dumps(data)
 
 if __name__ == "__main__":
-    application.run()
+	app.run()
